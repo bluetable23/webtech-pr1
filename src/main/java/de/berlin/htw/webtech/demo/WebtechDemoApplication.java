eef90d0f4ad14cdd2d3bf18038c79695e5ae8205
AAA <annalabchir@gmail.com>
2023-04-17 16:18:42 +0200

    initiales Projekt z

---
+++ src/main/java/de/berlin/htw/webtech/demo/WebtechDemoApplication.java
@@ -0,0 +1,13 @@
+package de.berlin.htw.webtech.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebtechDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebtechDemoApplication.class, args);
+	}
+
+}
